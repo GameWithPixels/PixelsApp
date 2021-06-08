08780add9ba88a0e9fb415c65deb996406fddb15
Olivier Basille <olivier@systemic-games.com>
2021-06-08 12:49:41 +0200

    Add custom logger with timestamps to help with troubleshooting

---
+++ Assets/Scripts/Utilities/CustomLogger.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class CustomLogger : MonoBehaviour
+{
+    struct LogEntry
+    {
+        public DateTime timestamp;
+        public int threadId;
+        public LogType type;
+        public string stackTrace;
+        public string message;
+    }
+
+    ConcurrentQueue<LogEntry> queue = new ConcurrentQueue<LogEntry>();
+    StreamWriter streamWriter;
+    StringBuilder stringBuilder = new StringBuilder();
+    string[] logTypesUpperCase = Enum.GetNames(typeof(LogType)).Select(s => s.Length < 4 ? s.ToUpper() + "\t" : s.ToUpper()).ToArray();
+
+    void Awake()
+    {
+#if !UNITY_EDITOR
+        enabled = false;
+        return;
+#endif
+        string filename = $"log_{DateTime.Now.ToString("o")}.txt";
+        foreach (char c in Path.GetInvalidFileNameChars())
+        {
+            filename = filename.Replace(c.ToString(), "_");
+        }
+        string pathname = Path.Combine(Application.persistentDataPath, filename);
+        Debug.Log("Writing logs to: " + pathname);
+        streamWriter = new StreamWriter(pathname);
+        streamWriter.WriteLine(filename);
+        streamWriter.WriteLine();
+        Application.logMessageReceivedThreaded += Application_logMessageReceivedThreaded;
+    }
+
+    void OnDisable()
+    {
+        streamWriter.Close();
+    }
+
+    void Application_logMessageReceivedThreaded(string condition, string stackTrace, LogType type)
+    {
+        queue.Enqueue(new LogEntry
+        {
+            timestamp = DateTime.Now,
+            threadId = System.Threading.Thread.CurrentThread.ManagedThreadId,
+            type = type,
+            stackTrace = stackTrace,
+            message = condition,
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (queue.TryDequeue(out LogEntry entry))
+        {
+            streamWriter.Write(entry.timestamp.ToString("o"));
+            streamWriter.Write("\t");
+            streamWriter.Write(entry.threadId);
+            streamWriter.Write("\t");
+            streamWriter.Write(logTypesUpperCase[(int)entry.type]);
+            streamWriter.Write("\t");
+            streamWriter.WriteLine(entry.message);
+            if ((entry.type != LogType.Log) && (entry.type != LogType.Warning))
+            {
+                // Skip first line in stack-trace (it's UnityEngine.Debug:Log) and add tab
+                var lines = entry.stackTrace.Split('\n');
+                for (int i = 1, iMax = lines.Length; i < iMax; ++i)
+                {
+                    string line = lines[i];
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        stringBuilder.Append('\t');
+                        stringBuilder.Append(line);
+                        stringBuilder.Append('\n');
+                    }
+                }
+                streamWriter.Write(stringBuilder);
+                stringBuilder.Clear();
+            }
+        }
+        streamWriter.Flush();
+    }
+}
