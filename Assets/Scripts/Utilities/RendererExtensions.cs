02fb15c2731f0a799ed9eee029fdafcfd076468b
Olivier Basille <olivier@systemic-games.com>
2021-07-01 15:40:02 +0200

    Optimize checking visibility when updating dice 3D rendering

--- Assets/Scripts/Utilities/RendererExtensions.cs
+++ Assets/Scripts/Utilities/RendererExtensions.cs
@@ -1,33 +1,40 @@
-﻿using UnityEngine;
+﻿using System.Threading;
+using UnityEngine;
 
 // Taken from this thread: https://forum.unity.com/threads/test-if-ui-element-is-visible-on-screen.276549/
 
 public static class RectTransformExtension
 {
+    const int numCorners = 4;
+    static ThreadLocal<Vector3[]> perThreadObjectCorners = new ThreadLocal<Vector3[]>(() => new Vector3[numCorners]);
+
     /// <summary>
     /// Counts the bounding box corners of the given RectTransform that are visible in screen space.
     /// </summary>
     /// <returns>The amount of bounding box corners that are visible.</returns>
     /// <param name="rectTransform">Rect transform.</param>
     /// <param name="camera">Camera. Leave it null for Overlay Canvasses.</param>
-    private static int CountCornersVisibleFrom(this RectTransform rectTransform, Camera camera = null)
+    /// <param name="stopOnFirstVisibleCorner">Set to either return 0 or 1 to save CPU cycles.</param>
+    private static int CountCornersVisibleFrom(this RectTransform rectTransform, Camera camera = null, bool stopOnFirstVisibleCorner = false)
     {
+        var objectCorners = perThreadObjectCorners.Value;
+
         Rect screenBounds = new Rect(0f, 0f, Screen.width, Screen.height); // Screen space bounds (assumes camera renders across the entire screen)
-        Vector3[] objectCorners = new Vector3[4];
         rectTransform.GetWorldCorners(objectCorners);
 
         int visibleCorners = 0;
-        Vector3 tempScreenSpaceCorner; // Cached
-        for (var i = 0; i < objectCorners.Length; i++) // For each corner in rectTransform
+        for (var i = 0; i < numCorners; i++) // For each corner in rectTransform
         {
-            if (camera != null)
-                tempScreenSpaceCorner = camera.WorldToScreenPoint(objectCorners[i]); // Transform world space position of corner to screen space
-            else
-                tempScreenSpaceCorner = objectCorners[i]; // If no camera is provided we assume the canvas is Overlay and world space == screen space
-
-            if (screenBounds.Contains(tempScreenSpaceCorner)) // If the corner is inside the screen
+            // If the corner is inside the screen
+            if (screenBounds.Contains(
+                camera?.WorldToScreenPoint(objectCorners[i]) // Transform world space position of corner to screen space
+                ?? objectCorners[i])) // If no camera is provided we assume the canvas is Overlay and world space == screen space
             {
                 visibleCorners++;
+                if (stopOnFirstVisibleCorner)
+                {
+                    return 1;
+                }
             }
         }
         return visibleCorners;
@@ -35,7 +42,7 @@ public static class RectTransformExtension
 
     /// <summary>
     /// Determines if this RectTransform is fully visible.
-    /// Works by checking if each bounding box corner of this RectTransform is inside the screen space view frustrum.
+    /// Works by checking if each bounding box corner of this RectTransform is inside the screen space view frustum.
     /// </summary>
     /// <returns><c>true</c> if is fully visible; otherwise, <c>false</c>.</returns>
     /// <param name="rectTransform">Rect transform.</param>
@@ -50,7 +57,7 @@ public static class RectTransformExtension
 
     /// <summary>
     /// Determines if this RectTransform is at least partially visible.
-    /// Works by checking if any bounding box corner of this RectTransform is inside the screen space view frustrum.
+    /// Works by checking if any bounding box corner of this RectTransform is inside the screen space view frustum.
     /// </summary>
     /// <returns><c>true</c> if is at least partially visible; otherwise, <c>false</c>.</returns>
     /// <param name="rectTransform">Rect transform.</param>
@@ -60,6 +67,6 @@ public static class RectTransformExtension
         if (!rectTransform.gameObject.activeInHierarchy)
             return false;
 
-        return CountCornersVisibleFrom(rectTransform, camera) > 0; // True if any corners are visible
+        return CountCornersVisibleFrom(rectTransform, camera, stopOnFirstVisibleCorner: true) > 0; // True if any corners are visible
     }
 }
